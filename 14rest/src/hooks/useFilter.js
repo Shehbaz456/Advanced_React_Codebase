336efafeaa374ed021067a83339e302f158f83b8
shehbaz <mdshehbaz666666@gmail.com>
2025-12-14 23:51:40 +0530

    implement filter  ( custom hook ) - useFilter

---
+++ 14rest/src/hooks/useFilter.js
@@ -0,0 +1,32 @@
+import {useState} from "react";
+function useFilter({expenses}) {
+      const [filter, setFilter] = useState("All");
+ let filteredExpenses = expenses.filter((item) => {
+    const itemDate = new Date(item.date);
+    const now = new Date();
+
+    if (filter === "Food" || filter === "Transport" || filter === "Shopping")
+      return item.category === filter;
+
+    // THIS MONTH
+    if (filter === "ThisMonth") {
+      return (
+        itemDate.getMonth() === now.getMonth() &&
+        itemDate.getFullYear() === now.getFullYear()
+      );
+    }
+
+    // LAST MONTH
+    if (filter === "LastMonth") {
+      const lastMonth = new Date(now.getFullYear(), now.getMonth() - 1);
+      return (
+        itemDate.getMonth() === lastMonth.getMonth() &&
+        itemDate.getFullYear() === lastMonth.getFullYear()
+      );
+    }
+    return true;
+  });
+  return {filteredExpenses,filter,setFilter}
+}
+
+export default useFilter
